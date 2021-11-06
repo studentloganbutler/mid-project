286b8dc884e3b9dd88711be1826da7c8ce23df7f
Logan Butler <logan.butler@student.swic.edu>
2021-11-06 18:51:54 -0500

    Add const for mongodb and add test router

--- server/app/router.js
+++ server/app/router.js
@@ -3,9 +3,16 @@ import config from "./config";
 import loader from "./loader";
 import {ObjectId} from "mongodb";
 
+const {
+    db: {name, collectionName},
+} = config;
 
 const router = new Router();
 
 // TODO: Add routes here (maybe 🤔 start with a GET test route)
+router.get("/", (_, res) => {
+    res.send("You have reached the Airbnb router!");
+});
+
 
 export default router;
